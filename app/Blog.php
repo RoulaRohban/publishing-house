8cde1e6b6305ec040d6c2424ed787595ac06883f
Yazan Alkahaleh <y.alkahaleh@aldrtechnology.com>
2021-05-25 23:58:32 +0300

    edit models

--- app/Blog.php
+++ app/Blog.php
@@ -10,8 +10,4 @@ class Blog extends Model
     protected $guarded = ['id', 'created_at', 'updated_at'];
     protected $hidden = ['created_at', 'updated_at'];
 
-    public function image()
-    {
-        return $this->morphOne(Image::class, 'imageable');
-    }
 }
